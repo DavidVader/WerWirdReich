2589436d9881b39d091514e5a03bd0eb15125173
David <david.mendel2212@gmail.com>
2026-01-09 08:07:38 +0100

    Added Pictures and some samples

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnNewGame = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            btnNewGame.Cursor = Cursors.Hand;
+            btnNewGame.FlatAppearance.BorderSize = 4;
+            btnNewGame.FlatStyle = FlatStyle.Flat;
+            btnNewGame.Font = new Font("Sans Serif Collection", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNewGame.Location = new Point(43, 279);
+            btnNewGame.Name = "btnNewGame";
+            btnNewGame.Size = new Size(340, 90);
+            btnNewGame.TabIndex = 0;
+            btnNewGame.Text = "NEW GAME";
+            btnNewGame.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(400, 9);
+            pictureBox1.Margin = new Padding(4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(349, 160);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1130, 712);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnNewGame);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "WER WIRD REICH";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnNewGame;
+        private PictureBox pictureBox1;
     }
 }

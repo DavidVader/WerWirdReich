f04108c1464af2554dc3e9c2f4be8085305263c4
David <david.mendel2212@gmail.com>
2026-01-09 08:55:20 +0100

    Retro Button Design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,16 +36,17 @@
             // 
             // btnNewGame
             // 
+            btnNewGame.BackgroundImage = (Image)resources.GetObject("btnNewGame.BackgroundImage");
+            btnNewGame.BackgroundImageLayout = ImageLayout.Stretch;
             btnNewGame.Cursor = Cursors.Hand;
-            btnNewGame.FlatAppearance.BorderSize = 4;
+            btnNewGame.FlatAppearance.BorderSize = 0;
             btnNewGame.FlatStyle = FlatStyle.Flat;
             btnNewGame.Font = new Font("Sans Serif Collection", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnNewGame.Location = new Point(59, 183);
-            btnNewGame.Margin = new Padding(2, 2, 2, 2);
+            btnNewGame.Location = new Point(74, 229);
+            btnNewGame.Margin = new Padding(2);
             btnNewGame.Name = "btnNewGame";
-            btnNewGame.Size = new Size(272, 72);
+            btnNewGame.Size = new Size(264, 93);
             btnNewGame.TabIndex = 0;
-            btnNewGame.Text = "NEW GAME";
             btnNewGame.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
@@ -53,24 +54,25 @@
             pictureBox1.BackColor = Color.Transparent;
             pictureBox1.BackgroundImageLayout = ImageLayout.None;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(320, 7);
+            pictureBox1.Location = new Point(400, 9);
+            pictureBox1.Margin = new Padding(4, 4, 4, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(279, 128);
+            pictureBox1.Size = new Size(349, 160);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(904, 570);
+            ClientSize = new Size(1130, 712);
             Controls.Add(pictureBox1);
             Controls.Add(btnNewGame);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "Form1";
             Text = "WER WIRD REICH";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();

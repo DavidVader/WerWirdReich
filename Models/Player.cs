ad5c917a5eed3eddc15eab61acc17edf6e50d725
David <david.mendel2212@gmail.com>
2026-01-20 22:49:07 +0100

    Created Data Models

---
+++ Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WerWirdReich.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int HighScore { get; set; }
+        public int AmountOfPlaythrough { get; set; }
+        public int TotalMoney { get; set; }
+    }
+}
---
+++ Models/Questions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WerWirdReich.Models
+{
+    public class Questions
+    {
+        public int Level { get; set; }
+        public string Question { get; set; }
+        public string[] Answers { get; set; }
+        public int RightAnswer { get; set; }
+    }
+}
